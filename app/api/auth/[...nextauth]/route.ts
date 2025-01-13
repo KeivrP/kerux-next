93c0646fad685c132bbe6e057c7d20eb8660b2c2
keiver <krpachecop@gmail.com>
2025-01-13 18:44:10 -0400

    hotfix(auth) change handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,11 @@
-import { handlers } from "@/auth";
+// app/api/auth/[...nextauth]/route.ts
+import { type NextRequest } from 'next/server';
+import { auth } from '@/auth';
 
-export const { GET, POST } = handlers;
+export const GET = auth((request: NextRequest) => {
+  return Response.json({ status: "ok" });
+}) as any;
+
+export const POST = auth((request: NextRequest) => {
+  return Response.json({ status: "ok" });
+}) as any;
